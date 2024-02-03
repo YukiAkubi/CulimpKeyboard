5845e94d33278b8c402a27384d98f3751aa248cf
YukiAkubi <kazuyuki19991207@gmail.com>
2024-02-03 20:19:41 +0900

    プロジェクト追加

---
+++ CulimpKeyboard/CulimpKeyboard/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CulimpKeyboard;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ CulimpKeyboard/CulimpKeyboard/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CulimpKeyboard;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
